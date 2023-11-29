5a33104798cd74453f39c383786ea40041541b4d
Asus <pamel4515@gmail.com>
2023-11-29 20:54:30 +0700

    pizza updatee lagi

---
+++ app/src/main/java/com/example/pizzaapp/activity_register.kt
@@ -0,0 +1,45 @@
+package com.example.pizzaapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class activity_register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    val txtEmail: EditText = findViewById(R.id.registerEmail)
+    val txtName: EditText = findViewById(R.id.registerPersonName)
+    val txtLevel: EditText = findViewById(R.id.registerLevel)
+    val txtPassword: EditText = findViewById(R.id.registerPassword)
+
+    val btnRegister: TextView = findViewById(R.id.buttonRegisterAccount)
+
+    btnRegister.setOnClickListener {
+        val databaseHelper = DatabaseHelper(this)
+
+        val email:String = txtEmail.text.toString().trim()
+        val name:String = txtName.text.toString().trim()
+        val level:String = txtLevel.text.toString().trim()
+        val password:String = txtPassword.text.toString().trim()
+
+        val data:String = databaseHelper.checkData(email)
+        if (data == ""){
+            databaseHelper.addAccount(
+                email, name, level, password)
+
+            val intentLogin = Intent(this@activity_register,
+                LoginActivity::class.java)
+            startActivity(intentLogin)
+        }else {
+            Toast.makeText(this@activity_register, "Register failed." +
+                    "Your email already registered", Toast.LENGTH_SHORT).show()
+        }
+    }
+    }
+}
---
+++ app/src/main/java/model/MenuModel.kt
@@ -0,0 +1,6 @@
+package model
+
+import android.graphics.Bitmap
+
+class MenuModel(var id:Int, var name:String, var price:Int, var image:Bitmap) {
+}
